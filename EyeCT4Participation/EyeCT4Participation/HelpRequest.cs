097858c20bf7f171299b5d8931fe33ac0ea0a661
TheBlade7c <rik_7@msn.com>
2016-04-13 09:07:32 +0200

    review en help request toegevoegd

--- EyeCT4Participation/EyeCT4Participation/HelpRequest.cs
+++ EyeCT4Participation/EyeCT4Participation/HelpRequest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,27 @@ namespace EyeCT4Participation
 {
     class HelpRequest
     {
+        public string Question { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public Boolean Urgency { get; set; }
+
+        public HelpRequest(string question, string description,DateTime date, bool urgency)
+        {
+            this.Question = question;
+            this.Description = description;
+            this.Date = date;
+            this.Urgency = Urgency;
+        }
+
+        public HelpRequest()
+        {
+
+        }
+
+        public void AddVolenteer(Account volenteer)
+        {
+
+        }
     }
 }
--- EyeCT4Participation/EyeCT4Participation/Review.cs
+++ EyeCT4Participation/EyeCT4Participation/Review.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,25 @@ namespace EyeCT4Participation
 {
     class Review
     {
+        public int Score { get; set; }
+        public string Description { get; set; }
+        public string Reaction { get; set; }
+
+        public Review(int score, string description, string reaction)
+        {
+            this.Score = score;
+            this.Description = description;
+            this.Reaction = reaction;
+        }
+
+        public Review()
+        {
+
+        }
+
+        public void AddReaction(string Message)
+        {
+
+        }
     }
 }
