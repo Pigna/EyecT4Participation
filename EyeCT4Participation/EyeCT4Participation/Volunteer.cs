e18996761206d0a37a595764baaabdb726dabcc2
Araxor <bvdsande93@gmail.com>
2016-04-14 19:00:05 +0100

    volunteer parameters

--- EyeCT4Participation/EyeCT4Participation/Volunteer.cs
+++ EyeCT4Participation/EyeCT4Participation/Volunteer.cs
@@ -10,7 +10,7 @@ namespace EyeCT4Participation
     {
         public bool Rijbewijs { get; set; }
 
-        public Volunteer(bool rijbewijs)
+        public Volunteer(bool rijbewijs,string username, string password, string name, string adress, string zipcode, string residence, string email, int phonenumber, int age, bool active, string geslacht) : base(username, password, name, adress, zipcode, residence, email, phonenumber, age,active, geslacht)
         {
             Rijbewijs = rijbewijs;
         }
