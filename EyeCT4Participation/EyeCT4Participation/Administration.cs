8df6407802a8dac9598bcc48aa3c1cbb02c25055
Myron <m.antonissen@student.fontys.nl>
2016-04-21 11:02:42 +0200

    Administration class update en database classen aangemaakt

--- EyeCT4Participation/EyeCT4Participation/Administration.cs
+++ EyeCT4Participation/EyeCT4Participation/Administration.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EyeCT4Participation.Database;
 
 namespace EyeCT4Participation
 {
@@ -12,6 +13,15 @@ namespace EyeCT4Participation
         public List<Chat> listChats = new List<Chat>();
         public List<HelpRequest> listHelprequests = new List<HelpRequest>();
         public List<Review> listReviews = new List<Review>();
+        private DBadministratie dbadministration = new DBadministratie();
+
+        public Administration()
+        {
+            ListAccount();
+            ListChat();
+            ListHelpRequest();
+            ListReview();
+        }
         public void AddNeedy(bool ov, string username, string password, string name, string adress, string zipcode, string residence, string email, int phonenumber, int age, string geslacht, bool active)
         {
             Needy newNeedy = new Needy(ov, username, password, name, adress, zipcode, residence, email, phonenumber, age, geslacht, active);
@@ -22,25 +32,48 @@ namespace EyeCT4Participation
             Volunteer newVolunteer = new Volunteer(licence, username, password, name, adress, zipcode, residence, email, phonenumber, age, geslacht, active);
             listAccounts.Add(newVolunteer);
         }
+        public void ListAccount()
+        {
+            listAccounts.Clear();
+            listAccounts = dbadministration.ListAccount();
+        }
 
+        public void ListChat()
+        {
+            listChats.Clear();
+            listChats = dbadministration.ListChat();
+        }
+        public void ListHelpRequest()
+        {
+            listHelprequests.Clear();
+            listHelprequests = dbadministration.ListHelpRequest();
+        }
+
+        public void ListReview()
+        {
+            listReviews.Clear();
+            listReviews = dbadministration.ListReview();
+        }
         public void DeactivateAccount(Account account)
         {
             account.Active = false;
+            dbadministration.DeactivateAccount(account);
         }
 
         public void DeactivateChat(Chat chat)
         {
-            
+            dbadministration.DeactivateChat(chat);
         }
 
         public void DeactivateHelpRequest(HelpRequest helprequest)
         {
             helprequest.Active = false;
+            dbadministration.DeactivateHelpRequest(helprequest);
         }
 
         public void DeactivateReview(Review review)
         {
-            
+            dbadministration.DeactivateReview(review);
         }
     }
 }
--- EyeCT4Participation/EyeCT4Participation/Database/DBadministratie.cs
+++ EyeCT4Participation/EyeCT4Participation/Database/DBadministratie.cs
@@ -8,6 +8,49 @@ namespace EyeCT4Participation.Database
 {
     class DBadministratie : DB
     {
+        public List<Account> ListAccount()
+        {
+            return null;
+        }
 
+        public List<Chat> ListChat()
+        {
+            return null;
+        }
+
+        public List<HelpRequest> ListHelpRequest()
+        {
+            return null;
+        }
+
+        public List<Review> ListReview()
+        {
+            return null;
+        }
+
+        public bool DeactivateAccount(Account account)
+        {
+            return false;
+        }
+
+        public bool DeactivateChat(Chat chat)
+        {
+            return false;
+        }
+
+        public bool DeactivateHelpRequest(HelpRequest helprequest)
+        {
+            return false;
+        }
+
+        public bool DeactivateReview(Review review)
+        {
+            return false;
+        }
+
+        public bool UpdateAccount(Account account)
+        {
+            return false;
+        }
     }
 }
