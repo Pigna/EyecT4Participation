61cb1878230bc6ae7bc199403b69cf81cc7153a5
Araxor <bvdsande93@gmail.com>
2016-04-21 11:08:20 +0100

    Chat

--- EyeCT4Participation/EyeCT4Participation/Chat.cs
+++ EyeCT4Participation/EyeCT4Participation/Chat.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 
 namespace EyeCT4Participation
 {
-        public class Chat : MainForm
+        public class Chat
     {
         public string Message;
         public DateTime Date;
@@ -23,29 +23,12 @@ namespace EyeCT4Participation
             Active = active;
         }
 
-        public Chat()
-        { 
-            
-        }
 
-        //public string TextValue
-        //{
-        //    get { return Message; }
-        //    set { Message = value; }
-        //}
-
-        public void AddMessage()
+        public override string ToString()
         {
-            lbChatConversation.Items.Add(tbChatMessage.Text);
-            lbChatConversation.Refresh();
-            tbChatMessage.Clear();
+            string info = Date + ": " + Message;
+            return info;
         }
-
-        //public override string ToString()
-        //{
-        //    string info = Date + ": " + Message;
-        //    return info;
-        //}
 
     }
 }
